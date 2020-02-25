29db3f8d7797ed4f6014033352a4c7c433d0eec5
Konrad Dawydiuk <Konrad.Dawydiuk.ctr@sabre.com>
2020-02-25 14:38:43 +0100

    Initial commit

---
+++ src/main/java/pl/dawydiuk/CarsdawRepairs/CarsdawRepairsApplication.java
@@ -0,0 +1,13 @@
+package pl.dawydiuk.CarsdawRepairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsdawRepairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsdawRepairsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dawydiuk/CarsdawRepairs/CarsdawRepairsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dawydiuk.CarsdawRepairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsdawRepairsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
