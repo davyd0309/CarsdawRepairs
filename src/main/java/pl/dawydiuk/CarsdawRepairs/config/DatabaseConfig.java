10507576a7e118ceebb7c005b0ee84e5ac1d930e
Konrad <konrad.dawydiuk@gmail.com>
2020-09-27 10:56:10 +0200

    add datebase config

---
+++ src/main/java/pl/dawydiuk/CarsdawRepairs/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package pl.dawydiuk.CarsdawRepairs.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+}
