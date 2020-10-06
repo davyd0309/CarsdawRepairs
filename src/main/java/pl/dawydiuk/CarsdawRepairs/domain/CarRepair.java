e1b66ae97675e09da5edd29176472196335cab22
Konrad <konrad.dawydiuk@gmail.com>
2020-10-06 14:01:18 +0200

    change table name car_repairs

--- src/main/java/pl/dawydiuk/CarsdawRepairs/domain/CarRepair.java
+++ src/main/java/pl/dawydiuk/CarsdawRepairs/domain/CarRepair.java
@@ -3,6 +3,7 @@ package pl.dawydiuk.CarsdawRepairs.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 import lombok.Builder;
@@ -10,6 +11,7 @@ import pl.dawydiuk.CarsdawRepairs.dto.CarRepairDto;
 
 @Entity
 @Builder
+@Table(name = "cars_repairs")
 class CarRepair {
 
     @Id
